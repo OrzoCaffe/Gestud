13d6f6af94d07fce3ab453e74783d2f4620f6c9d
ferraboneLuca <ferrabone.luca.93@gmail.com>
2015-12-17 16:59:26 +0100

    getter and Setter di pds e mat

--- src/main/java/it/xtphere/gestud/business/entity/Materia.java
+++ src/main/java/it/xtphere/gestud/business/entity/Materia.java
@@ -17,4 +17,33 @@ public class Materia implements Serializable {
     
     @Column
     private double voto;
+
+    public Materia() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVoto() {
+        return voto;
+    }
+
+    public void setVoto(double voto) {
+        this.voto = voto;
+    }
+    
+    
 }
