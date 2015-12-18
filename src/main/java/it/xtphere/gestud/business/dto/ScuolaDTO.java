8e668e774d7014425bf9c3b1d4b1c8ac3725c7a1
OrzoCaffe <enrico.argentieri@libero.it>
2015-12-18 09:41:25 +0100

    Dto package dto

--- src/main/java/it/xtphere/gestud/business/dto/ScuolaDTO.java
+++ src/main/java/it/xtphere/gestud/business/dto/ScuolaDTO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package it.xtphere.gestud.business.entity;
+package it.xtphere.gestud.business.dto;
 
 import java.io.Serializable;
 import java.util.List;
